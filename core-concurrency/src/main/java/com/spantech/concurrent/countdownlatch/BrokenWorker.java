a7a219a1e2c5c32336cb6833fb59aa27f527814c
Panyaprach Tularak <s5730213044@phuket.psu.ac.th>
2018-12-02 22:00:12 +0700

    add core concurrency

---
+++ core-concurrency/src/main/java/com/spantech/concurrent/countdownlatch/BrokenWorker.java
@@ -0,0 +1,27 @@
+package com.spantech.concurrent.countdownlatch;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ *
+ * @author Panyaprach Tularak
+ */
+public class BrokenWorker implements Runnable {
+    private final List<String> outputScraper;
+    private final CountDownLatch countDownLatch;
+
+    BrokenWorker(final List<String> outputScraper, final CountDownLatch countDownLatch) {
+        this.outputScraper = outputScraper;
+        this.countDownLatch = countDownLatch;
+    }
+
+    @Override
+    public void run() {
+        if (true) {
+            throw new RuntimeException("Oh dear");
+        }
+        countDownLatch.countDown();
+        outputScraper.add("Counted down");
+    }
+}
