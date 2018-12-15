27f049b4d68e7ec270635a33c83f83a5b4598ca1
Panyaprach Tularak <panyaprach.jop@gmail.com>
2018-12-16 00:20:05 +0700

    add basic https example

---
+++ core-java-lang/src/main/java/com/spantech/security/SSLExample.java
@@ -0,0 +1,19 @@
+package com.spantech.security;
+
+import java.net.URL;
+import javax.net.ssl.*;
+
+/**
+ *
+ * @author Panyaprach Tularak
+ */
+public class SSLExample {
+
+    public static void main(String[] args) throws Exception{
+        URL url = new URL("https://www.websecurity.symantec.com");
+        HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+        con.connect();
+        System.out.println(con.getContent().getClass());
+    }
+    
+}
